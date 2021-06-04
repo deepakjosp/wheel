806cfd5acae40a8ed4b8a72e64ac4145dffb9145
Deepak Jose <deepak.jose@bigbinary.com>
2021-06-04 18:47:53 +0530

    4 - Create list contacts ui  - added contacts navItem in sidebar  - added /contacts route  - added contact listing ui  - added contact table component

---
+++ app/javascript/src/constants/contacts.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    id: "5846499e-c369-11eb-8529-0242ac130003",
+    name: "Neeraj Singh",
+    email: "neeraj@bigbinary.com",
+    department: "Engineering",
+    contactNumber: "(555)-390-102",
+    addToBaseCamp: true,
+  },
+  {
+    id: "58464c3c-c369-11eb-8529-0242ac130003",
+    name: "Vinay Chandran",
+    email: "vinay@bigbinary.com",
+    department: "Engineering",
+    contactNumber: "99210011001",
+    addToBaseCamp: false,
+  },
+  {
+    id: "58464d2c-c369-11eb-8529-0242ac130003",
+    name: "Karthik Menon",
+    email: "karthik@bigbinary.com",
+    department: "Engineering",
+    contactNumber: "8283002301",
+    addToBaseCamp: true,
+  },
+  {
+    id: "58464df4-c369-11eb-8529-0242ac130003",
+    name: "Deepak Jose",
+    email: "deepak.jose@bigbinary.com",
+    department: "Engineering",
+    contactNumber: "9633249711",
+    addToBaseCamp: true,
+  },
+];
