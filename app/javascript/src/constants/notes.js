6fdeb381daabed5fd3cb1df1ad281dae978ddb77
Deepak Jose <deepak.jose@bigbinary.com>
2021-06-04 18:32:16 +0530

    3 Create new note UI  - added fields tag, assignee, dueDate to the create note form  - wrapped DateInput component inside formik field to use in newNoteform

--- app/javascript/src/constants/notes.js
+++ app/javascript/src/constants/notes.js
@@ -3,3 +3,33 @@ export const issueTypeBadgeColors = {
   agile: "green",
   bug: "red",
 };
+
+export const issueTypes = [
+  {
+    value: "internal",
+    label: "Internal",
+  },
+  {
+    value: "agileWorkFlow",
+    label: "Agile Work Flow",
+  },
+  {
+    value: "bug",
+    label: "Bug",
+  },
+];
+
+export const contacts = [
+  {
+    value: "vinay",
+    label: "Vinay",
+  },
+  {
+    value: "karthik",
+    label: "Karthik Menon",
+  },
+  {
+    value: "kabir",
+    label: "Kabir Nazir",
+  },
+];
